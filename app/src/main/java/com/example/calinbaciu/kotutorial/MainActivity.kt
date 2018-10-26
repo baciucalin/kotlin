277d408d695ab5b0f51b1da1cdbe1994142b9d79
Calin Baciu <calin.baciu@wirtek.com>
2018-10-26 18:22:39 +0300

    Changing to landscape doesnt reset score and timer

--- app/src/main/java/com/example/calinbaciu/kotutorial/MainActivity.kt
+++ app/src/main/java/com/example/calinbaciu/kotutorial/MainActivity.kt
@@ -10,10 +10,17 @@ class MainActivity : AppCompatActivity() {
 
     internal val INITAL_SCORE = 0
     internal var score = INITAL_SCORE
+    internal val initialCountDown: Long = 10000
+    internal val countDownInterval: Long = 1000
+
     internal lateinit var countDownTimer : CountDownTimer
     internal var gameStarted = false
-    internal val initialCountDown: Long = 5000
-    internal val countDownInterval: Long = 1000
+    internal var timeLeftOnTimer: Long = 60000
+
+    companion object {
+        private val SCORE_KEY = "SCORE_KEY"
+        private val TIME_LEFT_KEY = "TIME_LEFT_KEY"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +32,40 @@ class MainActivity : AppCompatActivity() {
         }
         gameScoreTv.text = getString(R.string.your_score, score.toString())
 
-        resetGame()
+        if(savedInstanceState != null){
+            score = savedInstanceState.getInt(SCORE_KEY)
+            timeLeftOnTimer = savedInstanceState.getLong(TIME_LEFT_KEY)
+            restoreGame()
+        }else{
+            resetGame()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(SCORE_KEY, score)
+        outState.putLong(TIME_LEFT_KEY, timeLeftOnTimer / 1000)
+        countDownTimer.cancel()
+
+    }
+
+    private fun restoreGame(){
+        gameScoreTv.text = getString(R.string.your_score,score.toString())
+        timeLeftTv.text = getString(R.string.time_left, timeLeftOnTimer.toString())
+
+        countDownTimer = object : CountDownTimer(timeLeftOnTimer *1000, countDownInterval){
+            override fun onFinish() {
+                endGame()
+            }
+
+            override fun onTick(p0: Long) {
+                timeLeftOnTimer = p0
+                val timeLeft =  p0 / 1000
+                timeLeftTv.text = getString(R.string.time_left, timeLeft.toString())
+            }
+        }
+        countDownTimer.start()
+        gameStarted = true
     }
 
     private fun incrementScore(){
@@ -59,6 +99,7 @@ class MainActivity : AppCompatActivity() {
         countDownTimer = object : CountDownTimer(initialCountDown, countDownInterval){
 
             override fun onTick(p0: Long) {
+                timeLeftOnTimer = p0
                 val timeLeft =  p0 / 1000
                 timeLeftTv.text = getString(R.string.time_left, timeLeft.toString())
             }

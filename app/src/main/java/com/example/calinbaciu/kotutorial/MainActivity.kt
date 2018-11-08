27c664a1f6536a3a16269a3d8dc3801e75a142bf
Calin Baciu <calin.baciu@wirtek.com>
2018-11-08 11:18:08 +0200

    Added about menu item, changed apps name

--- app/src/main/java/com/example/calinbaciu/kotutorial/MainActivity.kt
+++ app/src/main/java/com/example/calinbaciu/kotutorial/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.calinbaciu.kotutorial
 
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import android.view.animation.AlphaAnimation
 import android.view.animation.AnimationUtils
 import android.widget.Toast
@@ -121,6 +124,28 @@ class MainActivity : AppCompatActivity() {
             }
         }
         gameStarted = false
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+         super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.action_about){
+            showInfo()
+        }
+        return true
+    }
+
+    private fun showInfo(){
+        val dialogTitle = getString(R.string.about_dialog, BuildConfig.VERSION_NAME)
+        val dialogMessage = getString(R.string.about_message)
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(dialogTitle)
+        builder.setMessage(dialogMessage)
+        builder.create().show()
     }
 
 }
